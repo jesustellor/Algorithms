54617d7be040fa07d7df7dee7b5e2f01b94e0597
jesustellor <jesustellor@gmail.com>
2025-10-07 19:19:07 -0700

    binary searched explained as best i can

--- binarySearch.js
+++ binarySearch.js
@@ -16,3 +16,17 @@ function binarySearch(arr, target) {
 }
 
 console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 6));
+
+// in a list of 10 elements, the time complexity is O(log n)
+// log base 2 10 = 2 to the power of what equals 10
+// using the change of base formual, allows us to turn a logarithm to a different base, the formula is
+// log base b (a) = ln(a) / ln(b)
+// Using this formual our equation becomes
+// x = ln(10) / ln(2)             x = 2.302585 / 0.693147 = 3.321928
+// log base 2 10 = 3.321928 or approximately 3 computations to get to the target.
+// 2^3.321928 = 10 or close to it
+
+// ln(x) what power do i need to raise e to in order to get to x, where e = 2.718281828459045235 
+
+// ln(10) = 2.302585092994045684
+// ln(2) = 0.693147180559945309
