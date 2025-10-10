5b1e4705f011e713a7054932b775527a50605185
jesustellor <jesustellor@gmail.com>
2025-10-10 16:00:22 -0700

    practice / practice

--- linkedList-Array.js
+++ linkedList-Array.js
@@ -12,109 +12,118 @@ class LinkedList {
     this.size = 0;
   }
 
-// insert first nodes
-insertFirst(value) { 
-  this.head = new Node(value, this.head);
-  this.size++;
-}
+  // getNodeSize
+  size() {
+    let count = 0;
+    let current = this.head;
 
-// insert last node 
-insertLast(value) { 
- let node = new Node(value);
-  let current;
-  if (!this.head) {
-    this.head = node;
-  } else {
-    current = this.head;
-    while (current.next) {
+    while (current) {
+      count++;
       current = current.next;
     }
-    current.next = node;
+    return count;
   }
-}
 
-// insert at index
-insertAt(value, index) {
-  if(index > 0 && index > this.size) {
-    return;
+  // insert first nodes
+  insertFirst(value) {
+    this.head = new Node(value, this.head);
+    this.size++;
   }
-  if(index === 0) {
-    this.insertFirst(value);
-    return;
-  }
-  const node = new Node(value);
-  let current, previous;
-  current = this.head;
-  let count = 0;
-  while(count < index) {
-    previous = current;
-    current = current.next;
-    count++;
-  }
-  node.next = current;
-  previous.next = node;
-  this.size++;
-}
 
-// get at index
-getAt(index) {
-  let current = this.head;
-  let count = 0;
-  while (current) {
-    if (count === index) {
-      console.log(current.value);
+  // insert last node
+  insertLast(value) {
+    let node = new Node(value);
+    let current;
+    if (!this.head) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
     }
-    current = current.next;
-    count++;
   }
-return null;
-}
 
-// remove at index
-removeAt(index) {
-if(index > 0 && index > this.size) {
-  return;
-}
-let current = this.head;
-let previous;
-let count = 0;
-if(index === 0) {
-  this.head = current.next;
-} else {
-  while(count < index) {
-    count++;
-    previous = current;
-    current = current.next;
+  // insert at index
+  insertAt(value, index) {
+    if (index > 0 && index > this.size) {
+      return;
+    }
+    if (index === 0) {
+      this.insertFirst(value);
+      return;
+    }
+    const node = new Node(value);
+    let current, previous;
+    current = this.head;
+    let count = 0;
+    while (count < index) {
+      previous = current;
+      current = current.next;
+      count++;
+    }
+    node.next = current;
+    previous.next = node;
+    this.size++;
   }
-  previous.next = current.next;
-}
-this.size--;
-}
-// clear list
-clearList() {
-  this.head = null;
-  this.size = 0;
-}
-// print list data
-printListData() {
-  let current = this.head;
-  while (current) {
-    console.log(current.value);
-    current = current.next;
+
+  // get at index
+  getAt(index) {
+    let current = this.head;
+    let count = 0;
+    while (current) {
+      if (count === index) {
+        console.log(current.value);
+      }
+      current = current.next;
+      count++;
+    }
+    return null;
   }
-this.size++;
-}
+
+  // remove at index
+  removeAt(index) {
+    if (index > 0 && index > this.size) {
+      return;
+    }
+    let current = this.head;
+    let previous;
+    let count = 0;
+    if (index === 0) {
+      this.head = current.next;
+    } else {
+      while (count < index) {
+        count++;
+        previous = current;
+        current = current.next;
+      }
+      previous.next = current.next;
+    }
+    this.size--;
+  }
+  // clear list
+  clearList() {
+    this.head = null;
+    this.size = 0;
+  }
+  // print list data
+  printListData() {
+    let current = this.head;
+    while (current) {
+      console.log(current.value);
+      current = current.next;
+    }
+    this.size++;
+  }
 }
 
 const list = new LinkedList();
 list.insertFirst(10);
-list.insertFirst(20);
-list.insertFirst(30);
-list.insertLast(40);
-list.removeAt(2);
+
+console.log(list.size);
 
 list.printListData();
-
 
 // an array is a list fo elements that are stored contiguously in memory it looks like this
 
